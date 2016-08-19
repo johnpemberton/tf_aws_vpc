31e9a66d41f33fbdcc0e0b9fb6101e832676c476
John Pemberton <john.pemberton@sainsburys.co.uk>
2016-08-19 09:30:04 +0100

    separate private route table associations

--- main.tf
+++ main.tf
@@ -50,7 +50,7 @@ resource "aws_subnet" "public" {
 resource "aws_route_table_association" "private" {
   count = "${length(var.private_subnets)}"
   subnet_id = "${element(aws_subnet.private.*.id, count.index)}"
-  route_table_id = "${aws_route_table.private.id}"
+  route_table_id = "${element(aws_route_table.private.*.id, count.index)}"
 }
 
 resource "aws_route_table_association" "public" {
